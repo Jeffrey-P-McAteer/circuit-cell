b3244ac3243c503a29754cd7867b3ca2e0e5ac9e
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-03-07 12:33:29 -0500

    window hints++

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,13 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName("circuit-cell");
     QApplication::setOrganizationName("ExampleOrg");
 
+    // This is what sway should use to make the window float, if the user wants that (app_id=circuit-cell)
+    QGuiApplication::setDesktopFileName("circuit-cell");
+
     MainWindow window;
     window.resize(1024, 768);
+    window.setWindowState(Qt::WindowFullScreen);
+    window.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     window.show();
 
     return app.exec();
